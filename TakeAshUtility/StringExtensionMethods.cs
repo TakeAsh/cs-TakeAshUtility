5afff9fdbbe61815fff9f4a2ad1a5894b0465d21
TakeAsh <take@TakeAsh.net>
2015-07-10 01:48:06 +0900

    add StringExtensionMethods
    - TryParse<T>(this string text)
    - TryParse<T>(this string text, T defaultValue)
    - ToDefaultIfNullOrEmpty(this string text, string defaultValue = "")
    - SplitTrim(this string text, string[] separator = { "," },
    StringSplitOptions options = StringSplitOptions.RemoveEmptyEntries)

---
+++ TakeAshUtility/StringExtensionMethods.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace TakeAshUtility {
+
+    /// <summary>
+    /// String Extension Methods
+    /// </summary>
+    /// <remarks>
+    /// <list type="bullet">
+    /// <item>[C# - TryParse系のメソッドで一時変数を用意したくない… - Qiita](http://qiita.com/Temarin_PITA/items/9aac6c1f569fc2113e0d)</item>
+    /// </list>
+    /// </remarks>
+    public static class StringExtensionMethods {
+
+        /// <summary>
+        /// Convert string to object
+        /// </summary>
+        /// <typeparam name="T">target type</typeparam>
+        /// <param name="text">text to convert</param>
+        /// <returns>T type object</returns>
+        public static T TryParse<T>(this string text)
+            where T : struct {
+
+            return text.TryParse(default(T));
+        }
+
+        /// <summary>
+        /// Convert string to object
+        /// </summary>
+        /// <typeparam name="T">target type</typeparam>
+        /// <param name="text">text to convert</param>
+        /// <param name="defaultValue">return value if fail</param>
+        /// <returns>T type object</returns>
+        public static T TryParse<T>(this string text, T defaultValue)
+            where T : struct {
+
+            // コンバーターを作成
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+
+            // 変換不可能な場合は規定値を返す
+            if (!converter.CanConvertFrom(typeof(string))) {
+                return defaultValue;
+            }
+
+            try {
+                // 変換した値を返す
+                return (T)converter.ConvertFrom(text);
+            }
+            catch {
+                // 変換に失敗したら規定値を返す
+                return defaultValue;
+            }
+        }
+
+        /// <summary>
+        /// Return defalut value if text is null or empty
+        /// </summary>
+        /// <param name="text">text to test</param>
+        /// <param name="defaultValue">default value</param>
+        /// <returns>
+        /// <list type="table">
+        /// <item><term>text is neither null nor empty</term><description>text</description></item>
+        /// <item><term>text is null or empty</term><description>default value</description></item>
+        /// </list>
+        /// </returns>
+        public static string ToDefaultIfNullOrEmpty(this string text, string defaultValue = "") {
+            return !String.IsNullOrEmpty(text) ?
+                text :
+                defaultValue;
+        }
+
+        /// <summary>
+        /// Returns a string array that contains the substrings in this string that are delimited by elements of a specified string array.
+        /// </summary>
+        /// <param name="text">String to be splitted.</param>
+        /// <param name="separator">An array of single-character strings that delimit the substrings in this string. Defaut is ",".</param>
+        /// <param name="options">RemoveEmptyEntries to omit empty array elements from the array returned; or None to include empty array elements in the array returned.</param>
+        /// <returns>An array whose elements contain the substrings in this string that are delimited by one or more strings in separator.</returns>
+        public static IEnumerable<string> SplitTrim(
+            this string text,
+            string[] separator = null,
+            StringSplitOptions options = StringSplitOptions.RemoveEmptyEntries
+        ) {
+            separator = separator ?? new[] { "," };
+            return text.Split(separator, options)
+                .Select(item => item.Trim());
+        }
+    }
+}
