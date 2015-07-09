8856d95cc7960a5f53a2d7b67c9d0636a5bceee3
TakeAsh <take@TakeAsh.net>
2015-07-10 00:59:15 +0900

    add blank solution

---
+++ TakeAshUtility/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("TakeAshUtility")]
+[assembly: AssemblyDescription("C# basic utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TakeAsh.net")]
+[assembly: AssemblyProduct("TakeAshUtility")]
+[assembly: AssemblyCopyright("Copyright © TakeAsh.net 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("abb1805e-cad5-4a42-89b6-500e0fe6211b")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
