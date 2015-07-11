6f6d0f01ef8ed6859f37cb2aa4793112824b2060
TakeAsh <take@TakeAsh.net>
2015-07-11 15:17:40 +0900

    add ObjectExtensionMethods
    - SafeToString(this object obj, string valueIfNull = "")
    - SafeToString<TOpt1>(this object obj, TOpt1 opt1, string valueIfNull =
    "")
    - SafeToString<TOpt1, TOpt2>(this object obj, TOpt1 opt1, TOpt2 opt2,
    string valueIfNull = "")
    - SafeToString<TOpt1, TOpt2, TOpt3>(this object obj, TOpt1 opt1, TOpt2
    opt2, TOpt3 opt3, string valueIfNull = "")

---
+++ TakeAshUtility/ObjectExtensionMethods.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    /// <summary>
+    /// Safe ToString Method
+    /// </summary>
+    /// <remarks>
+    /// [c# - Checking for null before ToString() - Stack Overflow](http://stackoverflow.com/questions/550374/)
+    /// </remarks>
+    public static class ObjectExtensionMethods {
+
+        /// <summary>
+        /// check null before ToString()
+        /// </summary>
+        /// <param name="obj">object</param>
+        /// <param name="valueIfNull">value if object is null</param>
+        /// <returns>
+        /// <list type="table">
+        /// <item><term>object is not null</term><description>object.ToString()</description></item>
+        /// <item><term>object is null</term><description>valueIfNull</description></item>
+        /// </list>
+        /// </returns>
+        public static string SafeToString(this object obj, string valueIfNull = "") {
+            return obj != null ?
+                obj.ToString() :
+                valueIfNull;
+        }
+
+        /// <summary>
+        /// check null before ToString(TOpt1)
+        /// </summary>
+        /// <typeparam name="TOpt1">Type of option1</typeparam>
+        /// <param name="obj">object</param>
+        /// <param name="opt1">option1</param>
+        /// <param name="valueIfNull">value if object is null</param>
+        /// <returns>
+        /// <list type="table">
+        /// <item><term>object is not null</term><description>object.ToString(TOpt1)</description></item>
+        /// <item><term>object is null</term><description>valueIfNull</description></item>
+        /// </list>
+        /// </returns>
+        public static string SafeToString<TOpt1>(this object obj, TOpt1 opt1, string valueIfNull = "") {
+            if (obj == null) {
+                return valueIfNull;
+            }
+            var mi = obj.GetType().GetMethod("ToString", new[] { typeof(TOpt1), });
+            if (mi == null || mi.ReturnType != typeof(string)) {
+                throw new NotImplementedException(
+                    String.Format(
+                        "Not Implemented: {0}.ToString({1})",
+                        obj.GetType().Name, typeof(TOpt1).Name
+                    )
+                );
+            }
+            return (string)mi.Invoke(obj, new object[] { opt1, });
+        }
+
+        /// <summary>
+        /// check null before ToString(TOpt1, TOpt2)
+        /// </summary>
+        /// <typeparam name="TOpt1">Type of option1</typeparam>
+        /// <typeparam name="TOpt2">Type of option2</typeparam>
+        /// <param name="obj">object</param>
+        /// <param name="opt1">option1</param>
+        /// <param name="opt2">option2</param>
+        /// <param name="valueIfNull">value if object is null</param>
+        /// <returns>
+        /// <list type="table">
+        /// <item><term>object is not null</term><description>object.ToString(TOpt1, TOpt2)</description></item>
+        /// <item><term>object is null</term><description>valueIfNull</description></item>
+        /// </list>
+        /// </returns>
+        public static string SafeToString<TOpt1, TOpt2>(this object obj, TOpt1 opt1, TOpt2 opt2, string valueIfNull = "") {
+            if (obj == null) {
+                return valueIfNull;
+            }
+            var mi = obj.GetType().GetMethod("ToString", new[] { typeof(TOpt1), typeof(TOpt2), });
+            if (mi == null || mi.ReturnType != typeof(string)) {
+                throw new NotImplementedException(
+                    String.Format(
+                        "Not Implemented: {0}.ToString({1}, {2})",
+                        obj.GetType().Name, typeof(TOpt1).Name, typeof(TOpt2).Name
+                    )
+                );
+            }
+            return (string)mi.Invoke(obj, new object[] { opt1, opt2, });
+        }
+
+        /// <summary>
+        /// check null before ToString(TOpt1, TOpt2, TOpt3)
+        /// </summary>
+        /// <typeparam name="TOpt1">Type of option1</typeparam>
+        /// <typeparam name="TOpt2">Type of option2</typeparam>
+        /// <typeparam name="TOpt3">Type of option3</typeparam>
+        /// <param name="obj">object</param>
+        /// <param name="opt1">option1</param>
+        /// <param name="opt2">option2</param>
+        /// <param name="opt3">option3</param>
+        /// <param name="valueIfNull">value if object is null</param>
+        /// <returns>
+        /// <list type="table">
+        /// <item><term>object is not null</term><description>object.ToString(TOpt1, TOpt2, TOpt3)</description></item>
+        /// <item><term>object is null</term><description>valueIfNull</description></item>
+        /// </list>
+        /// </returns>
+        public static string SafeToString<TOpt1, TOpt2, TOpt3>(this object obj, TOpt1 opt1, TOpt2 opt2, TOpt3 opt3, string valueIfNull = "") {
+            if (obj == null) {
+                return valueIfNull;
+            }
+            var mi = obj.GetType().GetMethod("ToString", new[] { typeof(TOpt1), typeof(TOpt2), typeof(TOpt3), });
+            if (mi == null || mi.ReturnType != typeof(string)) {
+                throw new NotImplementedException(
+                    String.Format(
+                        "Not Implemented: {0}.ToString({1}, {2}, {3})",
+                        obj.GetType().Name, typeof(TOpt1).Name, typeof(TOpt2).Name, typeof(TOpt3).Name
+                    )
+                );
+            }
+            return (string)mi.Invoke(obj, new object[] { opt1, opt2, opt3, });
+        }
+    }
+}
---
+++ TakeAshUtility_Test/ObjectExtensionMethods_Test.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TakeAshUtility;
+
+namespace TakeAshUtility_Test {
+
+    [TestFixture]
+    class ObjectExtensionMethods_Test {
+
+        public enum Weekdays {
+            Sun,
+            Mon,
+            Tue,
+            Wed,
+            Thu,
+            Fri,
+            Sat,
+        }
+
+        [TestCase(null, null, null)]
+        [TestCase(null, "", "")]
+        [TestCase(null, "undefined", "undefined")]
+        [TestCase(1, null, "1")]
+        [TestCase(1, "", "1")]
+        [TestCase(1, "undefined", "1")]
+        [TestCase(2.2, null, "2.2")]
+        [TestCase(2.2, "", "2.2")]
+        [TestCase(2.2, "undefined", "2.2")]
+        [TestCase(Weekdays.Mon, null, "Mon")]
+        [TestCase(Weekdays.Mon, "", "Mon")]
+        [TestCase(Weekdays.Mon, "Null", "Mon")]
+        public void SafeToString_Test(object obj, string ifNull, string expected) {
+            var actual = obj.SafeToString(ifNull);
+            if (expected != null) {
+                Assert.AreEqual(expected, actual);
+            } else {
+                Assert.IsNull(actual);
+            }
+        }
+
+        [TestCase(null, "#,##0.000", null, null)]
+        [TestCase(null, "#,##0.000", "", "")]
+        [TestCase(null, "#,##0.000", "undefined", "undefined")]
+        [TestCase(2345.67, "#,##0.000", null, "2,345.670")]
+        [TestCase(2345.67, "#,##0.000", "", "2,345.670")]
+        [TestCase(2345.67, "#,##0.000", "undefined", "2,345.670")]
+        public void SafeToString_Opt1_String_Test(object obj, string opt1, string ifNull, string expected) {
+            var actual = obj.SafeToString(opt1, ifNull);
+            if (expected != null) {
+                Assert.AreEqual(expected, actual);
+            } else {
+                Assert.IsNull(actual);
+            }
+        }
+
+        [TestCase(null, "en-US", null, null)]
+        [TestCase(null, "en-US", "", "")]
+        [TestCase(null, "en-US", "undefined", "undefined")]
+        [TestCase(null, "ja-JP", null, null)]
+        [TestCase(null, "ja-JP", "", "")]
+        [TestCase(null, "ja-JP", "undefined", "undefined")]
+        [TestCase("2015/04/01", "en-US", null, "4/1/2015 12:00:00 AM")]
+        [TestCase("2015/04/01", "en-US", "", "4/1/2015 12:00:00 AM")]
+        [TestCase("2015/04/01", "en-US", "undefined", "4/1/2015 12:00:00 AM")]
+        [TestCase("2015/04/01", "ja-JP", null, "2015/04/01 00:00:00")]
+        [TestCase("2015/04/01", "ja-JP", "", "2015/04/01 00:00:00")]
+        [TestCase("2015/04/01", "ja-JP", "undefined", "2015/04/01 00:00:00")]
+        public void SafeToString_Opt1_CultureInfo_Test(string date, string opt1, string ifNull, string expected) {
+            DateTime? obj = null;
+            if (date != null) {
+                obj = DateTime.Parse(date);
+            }
+            var ci = new CultureInfo(opt1);
+            var actual = obj.SafeToString(ci, ifNull);
+            if (expected != null) {
+                Assert.AreEqual(expected, actual);
+            } else {
+                Assert.IsNull(actual);
+            }
+        }
+
+        [TestCase(null, "yyyy-MM-dd(ddd)", "en-US", null, null)]
+        [TestCase(null, "yyyy-MM-dd(ddd)", "en-US", "", "")]
+        [TestCase(null, "yyyy-MM-dd(ddd)", "en-US", "undefined", "undefined")]
+        [TestCase(null, "yyyy-MM-dd(ddd)", "ja-JP", null, null)]
+        [TestCase(null, "yyyy-MM-dd(ddd)", "ja-JP", "", "")]
+        [TestCase(null, "yyyy-MM-dd(ddd)", "ja-JP", "undefined", "undefined")]
+        [TestCase("2015/04/01", "yyyy-MM-dd(ddd)", "en-US", null, "2015-04-01(Wed)")]
+        [TestCase("2015/04/01", "yyyy-MM-dd(ddd)", "en-US", "", "2015-04-01(Wed)")]
+        [TestCase("2015/04/01", "yyyy-MM-dd(ddd)", "en-US", "undefined", "2015-04-01(Wed)")]
+        [TestCase("2015/04/01", "yyyy-MM-dd(ddd)", "ja-JP", null, "2015-04-01(水)")]
+        [TestCase("2015/04/01", "yyyy-MM-dd(ddd)", "ja-JP", "", "2015-04-01(水)")]
+        [TestCase("2015/04/01", "yyyy-MM-dd(ddd)", "ja-JP", "undefined", "2015-04-01(水)")]
+        public void SafeToString_Opt2_String_CultureInfo_Test(string date, string opt1, string opt2, string ifNull, string expected) {
+            DateTime? obj = null;
+            if (date != null) {
+                obj = DateTime.Parse(date);
+            }
+            var ci = new CultureInfo(opt2);
+            var actual = obj.SafeToString(opt1, ci, ifNull);
+            if (expected != null) {
+                Assert.AreEqual(expected, actual);
+            } else {
+                Assert.IsNull(actual);
+            }
+        }
+    }
+}
