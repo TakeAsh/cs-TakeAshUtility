5d44896b3b8e5baa19dc3b5deafd5abd47774589
TakeAsh <take@TakeAsh.net>
2015-07-15 23:54:17 +0900

    add ExceptionExtensionMethods
    - GetAllMessages(this Exception ex)

---
+++ TakeAshUtility/ExceptionExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public static class ExceptionExtensionMethods {
+
+        public static string GetAllMessages(this Exception ex) {
+            if (ex == null) {
+                return null;
+            }
+            var messages = new List<string>();
+            while (ex != null) {
+                messages.Add(ex.Message);
+                ex = ex.InnerException;
+            }
+            return String.Join("\n", messages);
+        }
+    }
+}
