4012dcf34a8ec5e6f8c53d9325d6644dec5bf5c8
TakeAsh <take@TakeAsh.net>
2015-07-15 09:16:45 +0900

    add FileFilter

---
+++ TakeAshUtility/FileFilter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public struct FileFilterItem {
+
+        public const string Separator = ";";
+
+        public string Description { get; private set; }
+        public IEnumerable<string> Patterns { get; private set; }
+
+        public FileFilterItem(string description, IEnumerable<string> patterns)
+            : this() {
+
+            Description = description;
+            Patterns = patterns.Select(pattern => Normalize(pattern));
+        }
+
+        public FileFilterItem(string description, string patterns)
+            : this(description, patterns.Split(new[] { Separator }, StringSplitOptions.RemoveEmptyEntries)) { }
+
+        public override string ToString() {
+            return Description + "|" + String.Join(Separator, Patterns);
+        }
+
+        public static string Normalize(string pattern) {
+            if (String.IsNullOrEmpty(pattern)) {
+                return null;
+            }
+            if (pattern.IndexOf('.') < 0) {
+                pattern = "." + pattern;
+            }
+            if (pattern[0] != '*') {
+                pattern = "*" + pattern;
+            }
+            return pattern;
+        }
+    }
+
+    public static class IEnumerableFileFilterItemExtensionMethods {
+
+        public static string ToFileFilter(this IEnumerable<FileFilterItem> list) {
+            return String.Join("|", list);
+        }
+
+        public static int GetIndex(this IEnumerable<FileFilterItem> list, string pattern) {
+            if (String.IsNullOrEmpty(pattern)) {
+                return -1;
+            }
+            pattern = FileFilterItem.Normalize(pattern).ToLower();
+            return list
+                .ToList()
+                .FindIndex(
+                    item => item
+                        .Patterns
+                        .Select(p => p.ToLower())
+                        .Contains(pattern)
+                );
+        }
+    }
+}
