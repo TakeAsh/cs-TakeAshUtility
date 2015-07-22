50282900ac104aab7f660ce0a5516ab9a594093f
TakeAsh <take@TakeAsh.net>
2015-07-22 13:38:35 +0900

    migrate GetAttribute<TAttr>() (Object -> Type)
    - migrate GetAttribute<TAttr>() from ObjectExtensionMethods to
    TypeExtensionMethods.

---
+++ TakeAshUtility/TypeExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public static class TypeExtensionMethods {
+
+        public static TAttr[] GetAttributes<TAttr>(this Type type, string propertyName)
+            where TAttr : Attribute {
+
+            if (type == null || String.IsNullOrEmpty(propertyName)) {
+                return null;
+            }
+            var memInfos = type.GetMember(propertyName);
+            if (memInfos == null || memInfos.Length == 0) {
+                return null;
+            }
+            var attrs = memInfos.First().GetCustomAttributes(typeof(TAttr), false) as TAttr[];
+            if (attrs == null || attrs.Length == 0) {
+                return null;
+            }
+            return attrs;
+        }
+
+        public static TAttr GetAttribute<TAttr>(this Type type, string propertyName)
+            where TAttr : Attribute {
+
+            var attrs = type.GetAttributes<TAttr>(propertyName);
+            return attrs == null || attrs.Length == 0 ?
+                null :
+                attrs.FirstOrDefault();
+        }
+    }
+}
---
+++ TakeAshUtility_Test/TypeExtensionMethods_Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TakeAshUtility;
+
+namespace TakeAshUtility_Test {
+
+    [TestFixture]
+    class TypeExtensionMethods_Test {
+
+        public class Sample1 {
+
+            [System.ComponentModel.Description("Job Name")]
+            public string JobName { get; set; }
+
+            [System.ComponentModel.Description("Create Date")]
+            public DateTime CreateDate { get; set; }
+        }
+
+        [TestCase("JobName", "Job Name")]
+        [TestCase("CreateDate", "Create Date")]
+        [TestCase("NotExist", null)]
+        public void GetAttribute_Test(string propertyName, string expected) {
+            var attr = typeof(Sample1).GetAttribute<System.ComponentModel.DescriptionAttribute>(propertyName);
+            if (expected != null) {
+                Assert.IsNotNull(attr);
+                Assert.AreEqual(expected, attr.Description);
+            } else {
+                Assert.IsNull(attr);
+            }
+        }
+    }
+}
