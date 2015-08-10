583c0ad91feee5f69b2f1a32200136641f6934c2
TakeAsh <take@TakeAsh.net>
2015-08-10 13:42:18 +0900

    add EventHandlerExtensionMethods
    # EventHandlerExtensionMethods
    - GetDelegate(this Object obj, string eventHandlerName)
    - GetHandler<T>(this Delegate delgate)
    # INotifyPropertyChangedExtensionMethods
    - remove GetDelegate(), GetHandler<T>()

---
+++ TakeAshUtility/EventHandlerExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TakeAshUtility {
+    
+    public static class EventHandlerExtensionMethods {
+
+        public static Delegate GetDelegate(this Object obj, string eventHandlerName) {
+            var flags = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField;
+            FieldInfo field;
+            return obj == null ||
+                String.IsNullOrEmpty(eventHandlerName) ||
+                (field = obj.GetType().GetField(eventHandlerName, flags)) == null ?
+                    null :
+                    field.GetValue(obj) as Delegate;
+        }
+
+        public static T GetHandler<T>(this Delegate delgate)
+            where T : class {
+
+            return delgate == null ?
+                null :
+                delgate.GetInvocationList()
+                    .Select(del => del as T)
+                    .Where(handler => handler != null)
+                    .FirstOrDefault();
+        }
+    }
+}
--- TakeAshUtility/INotifyPropertyChangedExtensionMethods.cs
+++ TakeAshUtility/INotifyPropertyChangedExtensionMethods.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
 
 namespace TakeAshUtility {
 
@@ -35,27 +34,6 @@ namespace TakeAshUtility {
             propertyNames.Where(name => !String.IsNullOrEmpty(name))
                 .ToList()
                 .ForEach(name => handler(sender, new PropertyChangedEventArgs(name)));
-        }
-
-        public static Delegate GetDelegate(this Object obj, string eventHandlerName) {
-            var flags = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField;
-            FieldInfo field;
-            return obj == null ||
-                String.IsNullOrEmpty(eventHandlerName) ||
-                (field = obj.GetType().GetField(eventHandlerName, flags)) == null ?
-                    null :
-                    field.GetValue(obj) as Delegate;
-        }
-
-        public static T GetHandler<T>(this Delegate delgate)
-            where T : class {
-
-            return delgate == null ?
-                null :
-                delgate.GetInvocationList()
-                    .Select(del => del as T)
-                    .Where(handler => handler != null)
-                    .FirstOrDefault();
         }
     }
 }
