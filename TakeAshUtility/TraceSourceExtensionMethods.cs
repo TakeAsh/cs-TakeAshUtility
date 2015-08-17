23cb59338f882621822b126146c3b2b4858ddf5e
TakeAsh <take@TakeAsh.net>
2015-08-17 16:07:38 +0900

    add TraceSourceExtensionMethods
    - TraceEventWithTimeAndMethodName()

---
+++ TakeAshUtility/TraceSourceExtensionMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualBasic.Logging;
+
+namespace TakeAshUtility {
+
+    public static class TraceSourceExtensionMethods {
+
+        const string DefaultDelimiter = "\t";
+
+        public static void TraceEventWithTimeAndMethodName(
+            this TraceSource source,
+            TraceEventType eventType,
+            string message,
+            int id = 0
+        ) {
+            if (source == null) {
+                return;
+            }
+            var listenr = source.Listeners
+                .OfType<FileLogTraceListener>()
+                .FirstOrDefault();
+            var delimiter = listenr != null ?
+                listenr.Delimiter :
+                DefaultDelimiter;
+            source.TraceEvent(
+                eventType,
+                id,
+                String.Join(delimiter, new[] {
+                    DateTime.Now.ToString("s"),
+                    new StackFrame(1).GetMethod().Name,
+                    message,
+                })
+            );
+        }
+    }
+}
