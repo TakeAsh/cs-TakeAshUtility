d8528aa643720aed47bd44a5f5d24ad97e8720c9
TakeAsh <take@TakeAsh.net>
2015-08-17 16:47:07 +0900

    add ApplicationSettingsBaseExtensionMethods
    - ToDetail()

---
+++ TakeAshUtility/ApplicationSettingsBaseExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public static class ApplicationSettingsBaseExtensionMethods {
+
+        public static string ToDetail(this ApplicationSettingsBase settings) {
+            if (settings == null) {
+                return null;
+            }
+            return String.Join(
+                ", ",
+                settings.GetType()
+                    .GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    .Where(pi => pi.GetIndexParameters().Length == 0)
+                    .Select(pi => pi.Name + ":{" + pi.GetValue(settings, null) + "}")
+            );
+        }
+    }
+}
