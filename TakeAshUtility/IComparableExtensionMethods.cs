818853ba93c5d85ab718501ff6428a43bfee8e7b
TakeAsh <take@TakeAsh.net>
2015-10-05 13:52:47 +0900

    add IComparableExtensionMethods
    - Clamp<T>(this T value, T min, T max, Comparer<T> comparer = null)

---
+++ TakeAshUtility/IComparableExtensionMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public static class IComparableExtensionMethods {
+
+        /// <summary>
+        /// Force a value to be in a range
+        /// </summary>
+        /// <typeparam name="T">the type of value</typeparam>
+        /// <param name="value">the value</param>
+        /// <param name="min">the minimum of the range</param>
+        /// <param name="max">the maximum of the range</param>
+        /// <param name="comparer">comparer of T</param>
+        /// <returns>
+        /// <list type="table">
+        /// <item><term>value &lt;= min</term><description>min</description></item>
+        /// <item><term>value &gt;= max</term><description>max</description></item>
+        /// <item><term>min &lt; value &amp;&amp; value &lt; max</term><description>value</description></item>
+        /// </list>
+        /// </returns>
+        /// <remarks>
+        /// [.net - How to force a number to be in a range in C#? - Stack Overflow](http://stackoverflow.com/questions/3176602/)
+        /// </remarks>
+        public static T Clamp<T>(this T value, T min, T max, Comparer<T> comparer = null)
+            where T : IComparable<T> {
+
+            comparer = comparer ?? Comparer<T>.Default;
+            if (comparer.Compare(value, min) <= 0) {
+                return min;
+            }
+            if (comparer.Compare(value, max) >= 0) {
+                return max;
+            }
+            return value;
+        }
+    }
+}
---
+++ TakeAshUtility_Test/IComparableExtensionMethods_Test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TakeAshUtility;
+
+namespace TakeAshUtility_Test {
+
+    [TestFixture]
+    class IComparableExtensionMethods_Test {
+
+        [TestCase(0, 0, 10, 0)]
+        [TestCase(-1, 0, 10, 0)]
+        [TestCase(-2, 0, 10, 0)]
+        [TestCase(-10, 0, 10, 0)]
+        [TestCase(1, 0, 10, 1)]
+        [TestCase(5, 0, 10, 5)]
+        [TestCase(10, 0, 10, 10)]
+        [TestCase(12, 0, 10, 10)]
+        [TestCase(15, 0, 10, 10)]
+        [TestCase(20, 0, 10, 10)]
+        [TestCase(0, -2, 15, 0)]
+        [TestCase(-1, -2, 15, -1)]
+        [TestCase(-2, -2, 15, -2)]
+        [TestCase(-10, -2, 15, -2)]
+        [TestCase(1, -2, 15, 1)]
+        [TestCase(5, -2, 15, 5)]
+        [TestCase(10, -2, 15, 10)]
+        [TestCase(12, -2, 15, 12)]
+        [TestCase(15, -2, 15, 15)]
+        [TestCase(20, -2, 15, 15)]
+        public void Clamp_int_Test(int val, int min, int max, int expected) {
+            Assert.AreEqual(expected, val.Clamp(min, max));
+        }
+
+        [TestCase(0, 0, 10, 0)]
+        [TestCase(-1, 0, 10, 0)]
+        [TestCase(-2, 0, 10, 0)]
+        [TestCase(-10, 0, 10, 0)]
+        [TestCase(1, 0, 10, 1)]
+        [TestCase(5, 0, 10, 5)]
+        [TestCase(10, 0, 10, 10)]
+        [TestCase(12, 0, 10, 10)]
+        [TestCase(15, 0, 10, 10)]
+        [TestCase(20, 0, 10, 10)]
+        [TestCase(0, -2, 15, 0)]
+        [TestCase(-1, -2, 15, -1)]
+        [TestCase(-2, -2, 15, -2)]
+        [TestCase(-10, -2, 15, -2)]
+        [TestCase(1, -2, 15, 1)]
+        [TestCase(5, -2, 15, 5)]
+        [TestCase(10, -2, 15, 10)]
+        [TestCase(12, -2, 15, 12)]
+        [TestCase(15, -2, 15, 15)]
+        [TestCase(20, -2, 15, 15)]
+        public void Clamp_double_Test(double val, double min, double max, double expected) {
+            Assert.AreEqual(expected, val.Clamp(min, max));
+        }
+    }
+}
