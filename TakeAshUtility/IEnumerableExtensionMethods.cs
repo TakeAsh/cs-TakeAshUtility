595d4baf7359f66f6298cebfcf33a423676bda3d
TakeAsh <take@TakeAsh.net>
2015-10-23 12:17:24 +0900

    add IEnumerableExtensionMethods
    - public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource>
    source, Func<TSource, TKey> selector)
    - public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource>
    source, Func<TSource, TKey> selector, IComparer<TKey> comparer)
    - public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource>
    source, Func<TSource, TKey> selector)
    - public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource>
    source, Func<TSource, TKey> selector, IComparer<TKey> comparer)
    - based on [morelinq - Extensions to LINQ to
    Objects](http://code.google.com/p/morelinq/)

---
+++ TakeAshUtility/IEnumerableExtensionMethods.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public static class IEnumerableExtensionMethods {
+
+        /// <summary>
+        /// Returns the minimal element of the given sequence,
+        /// based on the given projection.
+        /// </summary>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <typeparam name="TKey">Type of the projected element</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="selector">Selector to use to pick the results to compare</param>
+        /// <returns>The minimal element, according to the projection.</returns>
+        /// <remarks>
+        /// <list type="bullet">
+        /// <item>[morelinq - Extensions to LINQ to Objects - Google Project Hosting](http://code.google.com/p/morelinq/)</item>
+        /// <item>[c# - How to use LINQ to select object with minimum or maximum property value - Stack Overflow](http://stackoverflow.com/questions/914109/)</item>
+        /// <item>[c# - How to use linq to find the minimum - Stack Overflow](http://stackoverflow.com/questions/2736236/)</item>
+        /// </list>
+        /// </remarks>
+        public static TSource MinBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector
+        ) {
+            return source.MinBy(selector, Comparer<TKey>.Default);
+        }
+
+        /// <summary>
+        /// Returns the minimal element of the given sequence,
+        /// based on the given projection and the specified comparer for projected values.
+        /// </summary>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <typeparam name="TKey">Type of the projected element</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="selector">Selector to use to pick the results to compare</param>
+        /// <param name="comparer">Comparer to use to compare projected values</param>
+        /// <returns>The minimal element, according to the projection.</returns>
+        /// <remarks>
+        /// <list type="bullet">
+        /// <item>[morelinq - Extensions to LINQ to Objects - Google Project Hosting](http://code.google.com/p/morelinq/)</item>
+        /// <item>[c# - How to use LINQ to select object with minimum or maximum property value - Stack Overflow](http://stackoverflow.com/questions/914109/)</item>
+        /// <item>[c# - How to use linq to find the minimum - Stack Overflow](http://stackoverflow.com/questions/2736236/)</item>
+        /// </list>
+        /// </remarks>
+        public static TSource MinBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector,
+            IComparer<TKey> comparer
+        ) {
+            if (source == null || source.Count() == 0 || selector == null || comparer == null) {
+                return default(TSource);
+            }
+            using (var iterator = source.GetEnumerator()) {
+                iterator.MoveNext();
+                var candidate = iterator.Current;
+                var candidateKey = selector(candidate);
+                while (iterator.MoveNext()) {
+                    var next = iterator.Current;
+                    var nextKey = selector(next);
+                    if (comparer.Compare(candidateKey, nextKey) > 0) {
+                        candidate = next;
+                        candidateKey = nextKey;
+                    }
+                }
+                return candidate;
+            }
+        }
+
+        /// <summary>
+        /// Returns the maximal element of the given sequence,
+        /// based on the given projection.
+        /// </summary>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <typeparam name="TKey">Type of the projected element</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="selector">Selector to use to pick the results to compare</param>
+        /// <returns>The maximal element, according to the projection.</returns>
+        /// <remarks>
+        /// <list type="bullet">
+        /// <item>[morelinq - Extensions to LINQ to Objects - Google Project Hosting](http://code.google.com/p/morelinq/)</item>
+        /// <item>[c# - How to use LINQ to select object with minimum or maximum property value - Stack Overflow](http://stackoverflow.com/questions/914109/)</item>
+        /// <item>[c# - How to use linq to find the minimum - Stack Overflow](http://stackoverflow.com/questions/2736236/)</item>
+        /// </list>
+        /// </remarks>
+        public static TSource MaxBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector
+        ) {
+            return source.MaxBy(selector, Comparer<TKey>.Default);
+        }
+
+        /// <summary>
+        /// Returns the maximal element of the given sequence,
+        /// based on the given projection and the specified comparer for projected values.
+        /// </summary>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <typeparam name="TKey">Type of the projected element</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="selector">Selector to use to pick the results to compare</param>
+        /// <param name="comparer">Comparer to use to compare projected values</param>
+        /// <returns>The maximal element, according to the projection.</returns>
+        /// <remarks>
+        /// <list type="bullet">
+        /// <item>[morelinq - Extensions to LINQ to Objects - Google Project Hosting](http://code.google.com/p/morelinq/)</item>
+        /// <item>[c# - How to use LINQ to select object with minimum or maximum property value - Stack Overflow](http://stackoverflow.com/questions/914109/)</item>
+        /// <item>[c# - How to use linq to find the minimum - Stack Overflow](http://stackoverflow.com/questions/2736236/)</item>
+        /// </list>
+        /// </remarks>
+        public static TSource MaxBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector,
+            IComparer<TKey> comparer
+        ) {
+            if (source == null || source.Count() == 0 || selector == null || comparer == null) {
+                return default(TSource);
+            }
+            using (var iterator = source.GetEnumerator()) {
+                iterator.MoveNext();
+                var candidate = iterator.Current;
+                var candidateKey = selector(candidate);
+                while (iterator.MoveNext()) {
+                    var next = iterator.Current;
+                    var nextKey = selector(next);
+                    if (comparer.Compare(candidateKey, nextKey) < 0) {
+                        candidate = next;
+                        candidateKey = nextKey;
+                    }
+                }
+                return candidate;
+            }
+        }
+    }
+}
---
+++ TakeAshUtility_Test/IEnumerableExtensionMethods_Test.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TakeAshUtility;
+
+namespace TakeAshUtility_Test {
+
+    class Person {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person(int id, string name, DateTime dob) {
+            ID = id;
+            Name = name;
+            DateOfBirth = dob;
+        }
+
+        public override string ToString() {
+            return String.Join(", ", new[] {
+                "ID:" + ID,
+                "Name:{" + Name + "}",
+                "DateOfBirth:{" + DateOfBirth + "}" 
+            });
+        }
+    }
+
+    [TestFixture]
+    class IEnumerableExtensionMethods_Test {
+
+        private List<Person> _persons = new List<Person>() {
+            new Person(10000, "John", new DateTime(2000, 10, 1)),
+            new Person(10001, "Sam", new DateTime(2001, 2, 1)),
+            new Person(10002, "Jane", new DateTime(2000, 1, 1)),
+            new Person(10003, "Alice", new DateTime(2010, 4, 1)),
+            new Person(10004, "Bob", new DateTime(2020, 5, 1)),
+            new Person(10005, "Clarisse", new DateTime(2005, 6, 1)),
+        };
+
+        [TestCase("ID", 0)]
+        [TestCase("Name", 3)]
+        [TestCase("DateOfBirth", 2)]
+        public void MinBy_Test(string key, int expected) {
+            switch (key) {
+                case "ID":
+                    Assert.AreEqual(_persons[expected], _persons.MinBy(x => x.ID));
+                    break;
+                case "Name":
+                    Assert.AreEqual(_persons[expected], _persons.MinBy(x => x.Name));
+                    break;
+                case "DateOfBirth":
+                    Assert.AreEqual(_persons[expected], _persons.MinBy(x => x.DateOfBirth));
+                    break;
+            }
+        }
+
+        [TestCase("ID", 5)]
+        [TestCase("Name", 1)]
+        [TestCase("DateOfBirth", 4)]
+        public void MaxBy_Test(string key, int expected) {
+            switch (key) {
+                case "ID":
+                    Assert.AreEqual(_persons[expected], _persons.MaxBy(x => x.ID));
+                    break;
+                case "Name":
+                    Assert.AreEqual(_persons[expected], _persons.MaxBy(x => x.Name));
+                    break;
+                case "DateOfBirth":
+                    Assert.AreEqual(_persons[expected], _persons.MaxBy(x => x.DateOfBirth));
+                    break;
+            }
+        }
+    }
+}
