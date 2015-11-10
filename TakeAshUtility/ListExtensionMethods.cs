dbd3822c6cc2b2117c393566e053d56b4ad9293a
TakeAsh <take@TakeAsh.net>
2015-11-10 14:17:47 +0900

    add ListExtensionMethods
    - public static void SafeAdd<T>(this List<T> source, T item)

---
+++ TakeAshUtility/ListExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+    
+    public static class ListExtensionMethods {
+
+        public static void SafeAdd<T>(this List<T> source, T item) {
+            if (source == null || item == null) {
+                return;
+            }
+            source.Add(item);
+        }
+    }
+}
