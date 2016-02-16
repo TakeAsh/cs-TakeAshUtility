9cccb892b036e792a2c40aaa883e9f0257d03d42
TakeAsh <take@TakeAsh.net>
2016-02-16 11:32:13 +0900

    add ICollectionExtensionMethods
    - public static void AddRange<T>(this ICollection<T> collection,
    IEnumerable<T> source)
    - public static void SafeAddRange<T>(this ICollection<T> collection,
    IEnumerable<T> source)

---
+++ TakeAshUtility/ICollectionExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public static class ICollectionExtensionMethods {
+
+        public static void AddRange<T>(
+            this ICollection<T> collection,
+            IEnumerable<T> source
+        ) {
+            if (collection == null || source == null || source.Count() == 0) {
+                return;
+            }
+            source.ForEach(item => collection.Add(item));
+        }
+
+        public static void SafeAddRange<T>(
+            this ICollection<T> collection,
+            IEnumerable<T> source
+        ) where T : class {
+            if (collection == null || source == null || source.Count() == 0) {
+                return;
+            }
+            source.Where(item => item != null)
+                .ForEach(item => collection.Add(item));
+        }
+    }
+}
