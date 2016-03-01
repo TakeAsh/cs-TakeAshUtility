85410231b5cc8a90f1dba2aa382b73455eacc6fd
TakeAsh <take@TakeAsh.net>
2016-03-02 01:07:24 +0900

    GenericExtensionMethods: add Duplicate<T>()
    - public static void Duplicate<T>(this T destination, T source,
    IEnumerable<string> properties)

--- TakeAshUtility/GenericExtensionMethods.cs
+++ TakeAshUtility/GenericExtensionMethods.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using System.Text;
 
 namespace TakeAshUtility {
-    
+
     public static class GenericExtensionMethods {
 
         private const BindingFlags _flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly;
@@ -16,17 +16,34 @@ namespace TakeAshUtility {
         /// <typeparam name="T">Type of destination and source</typeparam>
         /// <param name="destination">Destination to be overwrited</param>
         /// <param name="source">Source of properties</param>
+        /// <remarks>
+        /// Properties to be Duplicated are Instance | Public | DeclaredOnly property.
+        /// </remarks>
         public static void Duplicate<T>(this T destination, T source) {
-            var _properties = typeof(T).GetProperties(_flags)
+            var properties = typeof(T).GetProperties(_flags)
                 .Where(property => property.CanWrite == true &&
                     property.GetSetMethod() != null &&
                     property.GetGetMethod() != null &&
                     property.GetIndexParameters().Length == 0
                 ).SafeToArray();
-            if (destination == null || source == null || _properties == null) {
+            if (destination == null || source == null || properties == null) {
                 return;
             }
-            _properties.ForEach(property => property.SetValue(destination, property.GetValue(source, null), null));
+            properties.ForEach(property => property.SetValue(destination, property.GetValue(source, null), null));
+        }
+
+        /// <summary>
+        /// Duplicate properties from source to destination.
+        /// </summary>
+        /// <typeparam name="T">Type of destination and source</typeparam>
+        /// <param name="destination">Destination to be overwrited</param>
+        /// <param name="source">Source of properties</param>
+        /// <param name="properties">Property names to be duplicated</param>
+        public static void Duplicate<T>(this T destination, T source, IEnumerable<string> properties) {
+            properties.ForEach(property => {
+                var pi = typeof(T).GetProperty(property, Type.EmptyTypes);
+                pi.SetValue(destination, pi.GetValue(source, null), null);
+            });
         }
     }
 }
