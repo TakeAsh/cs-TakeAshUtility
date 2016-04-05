e2f652232bebccdc37a084a7564c345182e997c5
TakeAsh <take@TakeAsh.net>
2016-04-05 13:42:31 +0900

    add BytesExtensionMethods
    - public static List<int> IndexesOf(this byte[] bytes, byte[] pattern,
    int startIndex = 0)
    - [c# - byte[] array pattern search - Stack
    Overflow](http://stackoverflow.com/questions/283456/)

---
+++ TakeAshUtility/BytesExtensionMethods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public static class BytesExtensionMethods {
+
+        /// <summary>
+        /// Reports the zero-based indexes of the specified pattern in this instance.
+        /// The search starts at a specified index.
+        /// </summary>
+        /// <param name="bytes">The byte array to be sought.</param>
+        /// <param name="pattern">The pattern to seek.</param>
+        /// <param name="startIndex">The search starting index.</param>
+        /// <returns>
+        /// <list type="bullet">
+        /// <item>The zero-based indexes of pattern, if the pattern is found.</item>
+        /// <item>null, if the pattern is not found.</item>
+        /// </list>
+        /// </returns>
+        /// <remarks>
+        /// [c# - byte[] array pattern search - Stack Overflow](http://stackoverflow.com/questions/283456/)
+        /// </remarks>
+        public static List<int> IndexesOf(
+            this byte[] bytes,
+            byte[] pattern,
+            int startIndex = 0
+        ) {
+            if (bytes == null ||
+                pattern == null ||
+                bytes.Length == 0 ||
+                pattern.Length == 0 ||
+                bytes.Length < pattern.Length) {
+                return null;
+            }
+            var indexes = new List<int>();
+            for (var i = startIndex; i < bytes.Length; ++i) {
+                if (!IsMatch(bytes, i, pattern)) {
+                    continue;
+                }
+                indexes.Add(i);
+            }
+            return indexes.Count == 0 ?
+                null :
+                indexes;
+        }
+
+        private static bool IsMatch(byte[] array, int position, byte[] pattern) {
+            if (pattern.Length > (array.Length - position)) {
+                return false;
+            }
+            for (int i = 0; i < pattern.Length; ++i) {
+                if (array[position + i] != pattern[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ TakeAshUtility_Test/BytesExtensionMethods_Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TakeAshUtility;
+
+namespace TakeAshUtility_Test {
+
+    [TestFixture]
+    class BytesExtensionMethods_Test {
+
+        byte[] data = new byte[] {
+            23, 36, 43, 76, 125, 56, 34, 234,
+            12, 3, 5, 76, 8, 0, 6, 125,
+            234, 56, 211, 122, 22, 4, 7, 89,
+            76, 64, 12, 3, 5, 76, 8, 0,
+            6, 125,
+        };
+        byte[] pattern = new byte[] {
+            12, 3, 5, 76, 8, 0, 6, 125,
+        };
+
+        [TestCase(0, "8 26")]
+        [TestCase(10, "26")]
+        public void IndexesOf_Test(int startIndex, string expected) {
+            var expected2 = expected.Split()
+                .Select(item => item.TryParse<int>())
+                .ToList();
+            var actual = data.IndexesOf(pattern, startIndex);
+            Assert.AreEqual(expected2, actual);
+        }
+    }
+}
