52f0fc267fc5ffec13fbca5b262c5149d7d0eaee
TakeAsh <take@TakeAsh.net>
2016-05-03 02:10:17 +0900

    add ConsoleEx
    - public static ConsoleKey ReadKey()

---
+++ TakeAshUtility/ConsoleEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public static class ConsoleEx {
+
+        /// <summary>
+        /// Non blocking ReadKey
+        /// </summary>
+        /// <returns>
+        /// <list type="bullet">
+        /// <item>default(ConsoleKey), if no key is pressed.</item>
+        /// <item>ConsoleKey, if some key is pressed.</item>
+        /// </list>
+        /// </returns>
+        public static ConsoleKey ReadKey() {
+            return !Console.KeyAvailable ?
+                default(ConsoleKey) :
+                Console.ReadKey(true).Key;
+        }
+    }
+}
