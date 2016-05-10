eca00dcc79ca20be93b3b27123568dc46a9ab23b
TakeAsh <take@TakeAsh.net>
2016-05-10 12:15:46 +0900

    IDictionaryExtensionMethods: fasten SafeGetValue()

--- TakeAshUtility/IDictionaryExtensionMethods.cs
+++ TakeAshUtility/IDictionaryExtensionMethods.cs
@@ -7,17 +7,35 @@ namespace TakeAshUtility {
 
     public static class IDictionaryExtensionMethods {
 
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+        /// <param name="dic">The dictionary to be scanned.</param>
+        /// <param name="key">The key of the value to get.</param>
+        /// <param name="defaultValue">The returned value, if the key is not found.</param>
+        /// <returns>
+        /// <list type="bullet">
+        /// <item>The value associated with the specified key, if the key is found.</item>
+        /// <item>The defaultValue, if the key is not found.</item>
+        /// </list>
+        /// </returns>
+        /// <remarks>
+        /// [c# - What is more efficient: Dictionary TryGetValue or ContainsKey+Item? - Stack Overflow](http://stackoverflow.com/questions/9382681/)
+        /// </remarks>
         public static TValue SafeGetValue<TKey, TValue>(
             this IDictionary<TKey, TValue> dic,
             TKey key,
             TValue defaultValue = default(TValue)
         ) {
-            if (dic == null ||
-                key == null ||
-                !dic.ContainsKey(key)) {
+            if (dic == null || key == null) {
                 return defaultValue;
             }
-            return dic[key];
+            TValue value;
+            return dic.TryGetValue(key, out value) ?
+                value :
+                defaultValue;
         }
     }
 }
