a7c2bd0a06cd176e626cca7ec230114474f00d24
TakeAsh <take@TakeAsh.net>
2016-05-10 09:46:27 +0900

    add IDictionaryExtensionMethods
    - public static TValue SafeGetValue<TKey, TValue>(this IDictionary<TKey,
    TValue> dic, TKey key, TValue defaultValue = default(TValue))

---
+++ TakeAshUtility/IDictionaryExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TakeAshUtility {
+
+    public static class IDictionaryExtensionMethods {
+
+        public static TValue SafeGetValue<TKey, TValue>(
+            this IDictionary<TKey, TValue> dic,
+            TKey key,
+            TValue defaultValue = default(TValue)
+        ) {
+            if (dic == null ||
+                key == null ||
+                !dic.ContainsKey(key)) {
+                return defaultValue;
+            }
+            return dic[key];
+        }
+    }
+}
