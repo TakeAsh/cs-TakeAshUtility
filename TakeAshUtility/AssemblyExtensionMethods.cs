d75c541afba20fa3a670e90864ce25391d5d002c
TakeAsh <take@TakeAsh.net>
2016-05-19 17:29:09 +0900

    add AssemblyExtensionMethods
    - public static T GetAttribute<T>(this Assembly assembly) where T :
    Attribute

---
+++ TakeAshUtility/AssemblyExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace TakeAshUtility {
+
+    public static class AssemblyExtensionMethods {
+
+        public static T GetAttribute<T>(this Assembly assembly)
+            where T : Attribute {
+
+            if (assembly == null) {
+                return null;
+            }
+            return Attribute.GetCustomAttribute(assembly, typeof(T)) as T;
+        }
+    }
+}
