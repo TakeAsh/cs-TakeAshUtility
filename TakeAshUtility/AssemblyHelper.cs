7fdbeb8c1ef4b1b0ce8c4fb397617d770896280a
TakeAsh <take@TakeAsh.net>
2016-06-30 14:25:33 +0900

    AssemblyHelper: add GetAssembly()

--- TakeAshUtility/AssemblyHelper.cs
+++ TakeAshUtility/AssemblyHelper.cs
@@ -16,5 +16,24 @@ namespace TakeAshUtility {
             }
             return Attribute.GetCustomAttribute(assembly, typeof(T)) as T;
         }
+
+        /// <summary>
+        /// Get Assembly that has specified name from CurrentDomain
+        /// </summary>
+        /// <param name="name">The name of Assembly</param>
+        /// <returns>
+        /// <list type="bullet">
+        /// <item>not null, if the Assembly is found.</item>
+        /// <item>null, if the Assembly is not found.</item>
+        /// </list>
+        /// </returns>
+        public static Assembly GetAssembly(string name) {
+            return String.IsNullOrEmpty(name) ?
+                null :
+                AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .Where(assembly => assembly.GetName().Name == name)
+                    .FirstOrDefault();
+        }
     }
 }
