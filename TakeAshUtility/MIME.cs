629a812b4bb57c70d9665c29e96383685dfd39f9
TakeAsh <take@TakeAsh.net>
2017-05-12 05:22:30 +0900

    add MIME

---
+++ TakeAshUtility/MIME.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace TakeAshUtility {
+
+    /// <summary>
+    /// Get Extension or MIME Type from registry.
+    /// </summary>
+    /// <remarks>
+    /// <list type="bullet">
+    /// <item>[C# Get file extension by content type - Stack Overflow](http://stackoverflow.com/questions/23087808/)</item>
+    /// <item>[Snippet: Mime types and file extensions - Articles and information on C# and .NET development topics - Cyotek](https://www.cyotek.com/blog/mime-types-and-file-extensions)</item>
+    /// </list>
+    /// </remarks>
+    public static class MIME {
+
+        public const string DefaultExtension = ".bin";
+        public const string DefaultMIMEType = "application/octet-stream";
+
+        /// <summary>
+        /// Get the default extension from the mime type.
+        /// </summary>
+        /// <param name="mimeType">the mime type.</param>
+        /// <returns>the default extension.</returns>
+        public static string GetDefaultExtension(string mimeType) {
+            if (String.IsNullOrEmpty(mimeType)) {
+                return DefaultExtension;
+            }
+            var key = Registry.ClassesRoot.OpenSubKey(@"MIME\Database\Content Type\" + mimeType, false);
+            var value = key == null ?
+                null :
+                key.GetValue("Extension", null);
+            return value == null ?
+                DefaultExtension :
+                value.ToString();
+        }
+
+        /// <summary>
+        /// Get the MIME Type from the file extension.
+        /// </summary>
+        /// <param name="extension">the file extension.</param>
+        /// <returns>the MIME Type.</returns>
+        public static string GetMimeTypeFromExtension(string extension) {
+            if (String.IsNullOrEmpty(extension)) {
+                return DefaultMIMEType;
+            }
+            if (!extension.StartsWith(".")) {
+                extension = "." + extension;
+            }
+            var key = Registry.ClassesRoot.OpenSubKey(extension, false);
+            var value = key == null ?
+                null :
+                key.GetValue("Content Type", null);
+            return value == null ?
+                DefaultMIMEType :
+                value.ToString();
+        }
+    }
+}
---
+++ TakeAshUtility_Test/MIME_Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TakeAshUtility;
+
+namespace TakeAshUtility_Test {
+
+    [TestFixture]
+    class MIME_Test {
+
+        public const string DefaultExtension = ".bin";
+        public const string DefaultMIMEType = "application/octet-stream";
+
+        [TestCase(null, DefaultExtension)]
+        [TestCase("", DefaultExtension)]
+        [TestCase("NotExists", DefaultExtension)]
+        [TestCase("text/plain", ".txt")]
+        [TestCase("text/html", ".htm")]
+        [TestCase("text/css", ".css")]
+        //[TestCase("application/javascript", ".js")]
+        [TestCase("image/jpeg", ".jpg")]
+        [TestCase("image/png", ".png")]
+        [TestCase("video/mp4", ".mp4")]
+        [TestCase("application/x-zip-compressed", ".zip")]
+        [TestCase(DefaultMIMEType, DefaultExtension)]
+        public void GetDefaultExtension_Test(string mimeType, string expected) {
+            var actual = MIME.GetDefaultExtension(mimeType);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(null, DefaultMIMEType)]
+        [TestCase("", DefaultMIMEType)]
+        [TestCase("NotExists", DefaultMIMEType)]
+        [TestCase(".txt", "text/plain")]
+        [TestCase(".htm", "text/html")]
+        [TestCase(".html", "text/html")]
+        [TestCase(".css", "text/css")]
+        [TestCase(".js", "application/javascript")]
+        [TestCase(".jpg", "image/jpeg")]
+        [TestCase(".jpeg", "image/jpeg")]
+        [TestCase(".png", "image/png")]
+        [TestCase(".mp4", "video/mp4")]
+        [TestCase(".zip", "application/x-zip-compressed")]
+        [TestCase(DefaultExtension, DefaultMIMEType)]
+        public void GetMimeTypeFromExtension_Test(string extension, string expected) {
+            var actual = MIME.GetMimeTypeFromExtension(extension);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
